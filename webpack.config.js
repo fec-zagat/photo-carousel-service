f845a7af058973de015211f946e7e5173762ea8d
Michael Su <xmichael.su@gmail.com>
2019-03-27 12:36:25 -0700

    Implement Webpack and React

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const CL_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    path: CL_DIR,
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+};
