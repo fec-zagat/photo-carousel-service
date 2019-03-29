a574e8ad1d3631fa83fddf0de39813180753f03e
Michael Su <xmichael.su@gmail.com>
2019-03-29 11:18:15 -0700

    Add Jest Test and Airbnb eslint

--- client/src/components/PhotoCarousel.jsx
+++ client/src/components/PhotoCarousel.jsx
@@ -1,15 +1,7 @@
 import React from 'react';
 
-class PhotoCarousel extends React.Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return (
-      <h1>Hello World!</h1>
-    );
-  }
+function PhotoCarousel() {
+  return <h1>Hello World!</h1>;
 }
 
 export default PhotoCarousel;
---
+++ tests/easy.test.js
@@ -0,0 +1,3 @@
+test('test that just works', () => {
+  expect(1).toBe(1);
+});
